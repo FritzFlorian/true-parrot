b1551026565e98f310d664e584784f8df21cf70a
FritzFlorian <codingtipp@gmail.com>
2016-11-22 17:47:57 +0100

    Add register action

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -9,3 +9,32 @@ exports.signup = {
   },
 
 };
+
+exports.register = {
+  auth: false,
+  validate: {
+
+    payload: User.validationSchema,
+
+    failAction: function (request, reply, source, error) {
+      reply.view('signup', {
+        title: 'Sign up error',
+        errors: error.data.details,
+      }).code(400);
+    },
+
+    options: {
+      abortEarly: false,
+    },
+  },
+
+  handler: function (request, reply) {
+    const user = new User(request.payload);
+
+    user.save().then(newUser => {
+      reply.redirect('/login');
+    }).catch(err => {
+      reply.redirect('/');
+    });
+  },
+};
