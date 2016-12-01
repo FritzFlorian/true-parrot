8157cfdfa97b05cfa83a06d9d92762aa18606d73
FritzFlorian <codingtipp@gmail.com>
2016-12-01 11:09:22 +0100

    Add basic admin dashboard layout

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -9,7 +9,35 @@ exports.dashboard = {
   },
 
   handler: function (request, reply) {
-    reply('you are an admin');
+    let tweets;
+    let tweetCount;
+    let userCount;
+
+    Tweet
+    .find({})
+    .sort({ createdAt: 'desc' })
+    .limit(5)
+    .populate('creator')
+    .exec()
+    .then((dbTweets) => {
+      tweets = dbTweets;
+
+      return Tweet.count({});
+    }).then((dbTweetCount) => {
+      tweetCount = dbTweetCount;
+
+      return User.count({});
+    }).then((dbUserCount) => {
+      userCount = dbUserCount;
+
+      return User.find({}).sort({ createdAt: 'desc' }).limit(5).exec();
+    }).then((users) => {
+      reply.view('adminDashboard',
+                  { users: users, tweets: tweets, userCount: userCount, tweetCount: tweetCount });
+    }).catch((error) => {
+      request.yar.flash('info', ['An internal error occurred, please try again.'], true);
+      reply.redirect('/tweets');
+    });
   },
 };
 
